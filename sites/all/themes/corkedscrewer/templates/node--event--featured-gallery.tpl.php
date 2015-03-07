bb0fde2b15161ac77f04be58d482367ecef122e0
const <vahkos@mail.ru>
2015-03-08 00:18:28 +0200

    gallery on main page is 50 percent width and with links to nodes

--- sites/all/themes/corkedscrewer/templates/node--event--featured-gallery.tpl.php
+++ sites/all/themes/corkedscrewer/templates/node--event--featured-gallery.tpl.php
@@ -1,3 +1,8 @@
-  <div class="slider-item-image"><?php echo render($content['field_field_carousel_image'])?></div>
+  <?php
+  $options = array('absolute' => TRUE);
+  $nid = 1; // Node ID
+  $url = url('node/' . $content["field_field_carousel_image"]['#object']->nid, $options);
+  ?>
+<div class="slider-item-image"><a href="<?php echo $url ?>"><?php echo render($content['field_field_carousel_image'])?></a></div>
   <div class="slider-item-title"><?php echo render($content['field_field_carousel_title'])?></div>
   <div class="slider-item-body"><?php echo render($content['field_field_carousel_text'])?></div>
